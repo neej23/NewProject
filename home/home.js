d1f72ac5bc000923a0ce70e0c166d2ed7b3114f7
Neej Savaliya <151989172+neej23@users.noreply.github.com>
2025-08-01 01:18:32 +0530

    new navigation bar

--- home/home.js
+++ home/home.js
@@ -47,3 +47,26 @@ function toggleDarkMode() {
     darkToggleIcon.classList.toggle('bi-sun');
   }
 }
+    const toggleIcon = document.getElementById('toggleIcon');
+    const menuOverlay = document.getElementById('menuOverlay');
+    const menuItems = menuOverlay.querySelectorAll('li');
+
+    
+
+    toggleIcon.addEventListener('click', () => {
+      const isOpen = menuOverlay.classList.toggle('show');
+
+      toggleIcon.classList.toggle('fa-chevron-down', !isOpen);
+      toggleIcon.classList.toggle('fa-angle-up', isOpen);
+    
+      toggleIcon.style.color = '#333';
+    
+      if (isOpen) {
+        menuItems.forEach((item) => {
+
+          item.classList.remove('animate');
+          void item.offsetWidth; // force reflow
+          setTimeout(() => item.classList.add('animate'), 50);
+        });
+      } 
+    });
